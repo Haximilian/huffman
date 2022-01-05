e86f495df45b679ba8ba217e5be7908a7fa2f8a6
Haximilian <47073524+Haximilian@users.noreply.github.com>
2022-01-05 08:05:01 -0800

    read entire string into memory... find length using stat call

--- huffman.c
+++ huffman.c
@@ -4,23 +4,42 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/stat.h>
 
-#define BUFFER_SIZE 1024
 #define CHAR_SIZE 128
 
-char buffer[BUFFER_SIZE];
+typedef struct m_string m_string_t;
+struct m_string {
+    int size;
+    char str[0];
+};
+
+m_string_t* file_to_string(char* filename) {
+    struct stat s;
+    
+    int descriptor = open(filename, O_RDONLY);
+
+    fstat(descriptor, &s);
+
+    m_string_t* to_return = malloc(sizeof(m_string_t) + s.st_size + 1);
+
+    to_return->str[s.st_size] = '\0';
+
+    to_return->size = read(descriptor, &(to_return->str), s.st_size);
+
+    return(to_return);
+}
 
 int main(int argc, char** argv) {
     char* filename = argv[1];
-    int fd = open(filename, O_RDONLY);
-    int size = read(fd, buffer, BUFFER_SIZE);
-    buffer[size] = '\0';
+
+    m_string_t* str = file_to_string(filename);
 
     int freq_table[CHAR_SIZE];
     memset(freq_table, 0, CHAR_SIZE * sizeof(int));
 
-    for (int i = 0; i < size; i++) {
-        freq_table[buffer[i]]++;
+    for (int i = 0; i < str->size; i++) {
+        freq_table[str->str[i]]++;
     }
 
     heap_t* heap = create_heap(CHAR_SIZE);
