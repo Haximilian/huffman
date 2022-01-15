01554d1c0c734f9b323b5bee447d35ccd9c5cdee
maximilian <Haximilian>
2022-01-14 18:09:46 -0800

    define huffman tree structs

---
+++ huffman.h
@@ -0,0 +1,18 @@
+typedef huffman_type_t;
+enum huffman_type {
+    internal = 0, 
+    external
+};
+
+typedef struct huffman huffman_t;
+struct huffman {
+    int freq;
+    huffman_type_t type; 
+    union {
+        struct {
+            huffman_t* l;
+            huffman_t* r;
+        } child;
+        char ch;
+    } val;
+};
